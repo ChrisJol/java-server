16cf26191c4580660a36f96e1fabf9885136ca8e
Chris Jol <jol.chris2@gmail.com>
2019-09-11 18:07:08 -0700

    Added WebServer.java to root directory

---
+++ WebServer.java
@@ -0,0 +1,6 @@
+public class WebServer {
+    public static void main(string[] args) {
+        //TODO: Start Server
+        //This class won't do much other than just creating an instance of and running the server
+    }
+}
