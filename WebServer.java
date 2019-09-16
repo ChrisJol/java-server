58fc7ac1e5d861c5f92cdf9bc0612d0ddd7153fd
Chris Jol <jol.chris2@gmail.com>
2019-09-16 15:08:22 -0700

    added import statements to WebServer.java

--- WebServer.java
+++ WebServer.java
@@ -1,5 +1,8 @@
+import java.net.*;
+import java.io.*;
+
 public class WebServer {
-    public static void main(string[] args) {
+    public static void main(String[] args) {
         //TODO: Start Server
         //This class won't do much other than just creating an instance of and running the server
     }
