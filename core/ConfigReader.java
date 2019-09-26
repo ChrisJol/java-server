a8ef738e2af951db800af7899d491b4a11de7a09
Nicole <nicolebernardo96@gmail.com>
2019-09-25 17:59:04 -0700

    re-wrote data structure for config files/properties and parse method

--- core/ConfigReader.java
+++ core/ConfigReader.java
@@ -1,15 +1,18 @@
 package core;
 
 import java.io.*;
-import java.util.Properties;
+import java.util.*;
 
 public class ConfigReader {
     private static ConfigReader single_instance = null; //singleton instance
+
+    Map<String, String> configParams = new HashMap<String, String>();
 
     private File configFile;
     private String serverRoot;
     private String docRoot;
     private String logFile;
+    private String alias;
     private int defaultPort;
 
     private ConfigReader(String fileName){
@@ -26,37 +29,80 @@ public class ConfigReader {
     }
 
     public String getServerRoot(){
-        return serverRoot;
+        return configParams.get("ServerRoot");
     }
 
     public String getDocRoot(){
-        return docRoot;
+        return configParams.get("DocumentRoot");
     }
 
     public String getLogFile(){
-        return logFile;
+        return configParams.get("LogFile");
     }
 
     public int getDefaultPort(){
-        return defaultPort;
+        return Integer.parseInt(configParams.get("Listen"));
     }
 
-    public void load(){
-        try{
-            FileReader reader = new FileReader(configFile);
-            Properties properties = new Properties();
-            properties.load(reader);
+    public String getAlias(String alias){
+        return configParams.get(alias);
+    }
 
-            serverRoot = properties.getProperty("ServerRoot").replace("\"", "");
-            docRoot = properties.getProperty("DocumentRoot").replace("\"", "");
-            logFile = properties.getProperty("LogFile").replace("\"", "");
-            defaultPort = Integer.parseInt(properties.getProperty("Listen"));
+    private void load(){
+        try {
+            FileReader fileReader = new FileReader(configFile);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String property = reader.readLine();
 
+            while(property != null) {
+                String[] properties = property.split(" ");
+
+                if(properties[0].equals("Alias") || properties[0].equals("ScriptAlias")) {
+                    configParams.put(properties[1], properties[2].replace("\"", ""));
+                } else {
+                    configParams.put(properties[0], properties[1].replace("\"", ""));
+                }
+                property = reader.readLine();
+            }
             reader.close();
-        }catch(FileNotFoundException e){
+
+            System.out.println(getServerRoot());
+            System.out.println(getDocRoot());
+            System.out.println(getLogFile());
+            System.out.println(getDefaultPort());
+            System.out.println(getAlias("/ab/"));
+            System.out.println(getAlias("/cgi-bin/"));
+            System.out.println(getAlias("/~traciely/"));
+        }
+        catch(FileNotFoundException e){
             System.out.println("File not found in classpath");
-        }catch(IOException e){
+        }
+        catch(IOException e){
             e.printStackTrace();
         }
+
+
+        // try{
+        //     FileReader reader = new FileReader(configFile);
+        //     Properties properties = new Properties();
+        //     properties.load(reader);
+
+        //     serverRoot = properties.getProperty("ServerRoot").replace("\"", "");
+        //     docRoot = properties.getProperty("DocumentRoot").replace("\"", "");
+        //     logFile = properties.getProperty("LogFile").replace("\"", "");
+        //     alias = properties.getProperty("Alias").replace("\"", "");
+        //     defaultPort = Integer.parseInt(properties.getProperty("Listen"));
+
+        //     System.out.println(alias);
+
+        //     reader.close();
+
+        // }catch(FileNotFoundException e){
+        //     System.out.println("File not found in classpath");
+        // }catch(IOException e){
+        //     e.printStackTrace();
+        // }
+
+
     }
 }
